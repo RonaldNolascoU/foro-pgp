074358ffb70736d47f3396715f4f0391605ebf6b
William <william@trezlabs.com>
2021-05-14 22:25:01 -0600

    components added

--- web/components/cards/CourseInfo.vue
+++ web/components/cards/CourseInfo.vue
@@ -20,10 +20,10 @@
                 .w-20.text-center
                     i.sprites.phone
                 .ms-1.w-75.text-nowrap Disponible también en celuar
-            .secondary.secondary-font.mt-0.mb-3.d-flex.mx-0.align-items-center
+            .secondary.secondary-font.mt-0.mb-3.d-flex.mx-0.align-items-start
                 .w-20.text-center
                     i.sprites.message
-                .ms-1.w-75.text-nowrap Foro de discusión con las abogadas
+                .ms-1.w-75 Foro de discusión con las abogadas
 
             .d-flex.justify-content-center.pt-4
                 nuxt-link.secondary-btn.text-light.btn-radius.text-decoration-none.px-2.btn-text-size-lg.text-nowrap.px-4(to='/take-course')
--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -4,6 +4,33 @@
       :title="$store.state.eventInformation.name"
       :ticket-link="$store.state.eventInformation.ticket"
     )
+    .d-flex.justify-content-center
+      PostCard
+    br
+    br
+    .d-flex.justify-content-center
+      EventInfo
+    br
+    br
+    .d-flex.justify-content-center
+      UserInfo
+    br
+    br
+    .d-flex.justify-content-center
+      Temary
+    br
+    br
+    .d-flex.justify-content-center
+      Feedback
+    br
+    br
+    .d-flex.justify-content-center
+      CourseInfo
+    br
+    br
+    .d-flex.justify-content-center
+      FaqInfo
+
     .row.m-0
       Sidebar
       nuxt.layout
@@ -14,19 +41,34 @@
 import Navbar from '~/components/Navbar.vue'
 import Footer from '~/components/Footer.vue'
 import Sidebar from '../components/Sidebar.vue'
+import CourseInfo from '../components/cards/CourseInfo'
+import FaqInfo from '../components/cards/FaqInfo'
+import Feedback from '../components/cards/Feedback'
+import Temary from '../components/cards/Temary'
+import UserInfo from '../components/cards/UserInfo'
+import EventInfo from '../components/cards/EventInfo'
+import PostCard from '../components/cards/PostCard'
 
 export default {
   components: {
     Navbar,
     Footer,
-    Sidebar
+    Sidebar,
+    CourseInfo,
+    FaqInfo,
+    Feedback,
+    Temary,
+    UserInfo,
+    EventInfo,
+    PostCard
   }
 }
 </script>
 
-<style>
-@import '../styles/custom-media.css';
-@import '../styles/custom-properties.css';
+<style lang="scss">
+// @import '../styles/custom-media.css';
+// @import '../styles/custom-properties.css';
+@import '../styles/app';
 
 html {
   font-family: var(--font-family-sans);
