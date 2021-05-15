8e0ca91a26c080814355e4be009b1b7615ef87a8
William <william@trezlabs.com>
2021-05-15 16:09:25 -0600

    slider added

--- web/components/RecentPosts.vue
+++ web/components/RecentPosts.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
 div
-  Post
+  VueSlickCarousel(v-bind="settings")
+      div 
+        Post
+      div 
+        Post
+      div 
+        Post
+      div 
+        Post
 </template>
 
 <script>
 import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import Post from '@/components/Post'
 export default {
   components: { VueSlickCarousel, Post },
@@ -14,7 +24,7 @@ export default {
         dots: true,
         dotsClass: 'slick-dots custom-dot-class',
         edgeFriction: 0.35,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
